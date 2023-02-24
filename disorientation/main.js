ab223bb065024715799824710ef31c5d6bb49b28
Christine Brovkina <christine@whateverism.de>
2023-02-24 17:30:09 +0100

    Add disorientation page

---
+++ disorientation/main.js
@@ -0,0 +1,6 @@
+import '../style.scss'
+import { createHeader } from '../components/header.js'
+import { createNav } from '../components/navigation.js'
+
+createHeader(document.querySelector('body'))
+createNav(document.querySelector('body'))
