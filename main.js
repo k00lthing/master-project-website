691fb9f89cd8a8f34ac763c49e47171b1492c94b
Christine Brovkina <christine@whateverism.de>
2023-02-24 15:26:25 +0100

    Sassify

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import './style.scss'
+import { createHeader } from './header.js'
+import { createNav } from './navigation.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+createHeader(document.querySelector('body')) 
+createNav(document.querySelector('body'))
