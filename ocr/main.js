16833e7a9a7adadaca158c09d9560821c6e98077
Christine Brovkina <christine@whateverism.de>
2023-02-24 15:26:48 +0100

    Add OCR page

---
+++ ocr/main.js
@@ -0,0 +1,6 @@
+import './style.scss'
+import { createHeader } from './header.js'
+import { createNav } from './navigation.js'
+
+createHeader(document.querySelector('body'))
+createNav(document.querySelector('body'))
