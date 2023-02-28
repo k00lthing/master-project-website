115101536f97eeb56606b92109a87d87abbea83d
Christine Brovkina <christine@whateverism.de>
2023-02-28 20:31:09 +0100

    Implement ocr with tesseract.js

---
+++ components/ocr.js
@@ -0,0 +1,60 @@
+import Tesseract from 'tesseract.js'
+
+export function createOCR(element) {
+
+    element.insertAdjacentHTML('beforeend',
+        `
+        <p>Note: The image format supported by Tesseract are jpg, png, bmp and pbm. </p>
+    <select id="lang">
+        <option value='eng' selected> English </option>
+        <option value='deu'> German </option>
+    </select>
+  <input type="file" id="file-img" accept="image/*" />
+  <img id="selected-image" src="" />
+  <div id="log">
+    <button id="start-ocr">Recognize text</button>
+  </div>
+    `)
+
+    const button = document.querySelector("#start-ocr")
+    const langSelector = document.querySelector('#lang')
+    const imgSelector = document.querySelector('#file-img')
+    const imgPreview = document.querySelector('#selected-image')
+    console.log(imgPreview)
+
+    let language = langSelector.value
+    let image = null
+
+    imgSelector.addEventListener("change", handleFiles, false)
+
+    function handleFiles() {
+        const fileList = this.files /* now you can work with the file list */
+        image = fileList[0]
+        const objectURL = window.URL.createObjectURL(fileList[0])
+       
+        imgPreview.src = objectURL
+
+    }
+
+    button.addEventListener('click', (event) => {
+
+        Tesseract.recognize(
+            image, language,
+            {
+                logger: m => console.log(m)
+            }
+        )
+            .catch(err => {
+                console.error(err)
+            })
+            .then(result => {
+                console.log(result)
+            })
+
+    })
+
+}
+
+
+
+
--- ocr/main.js
+++ ocr/main.js
@@ -1,6 +1,8 @@
 import '../style.scss'
 import { createHeader } from '../components/header.js'
 import { createNav } from '../components/navigation.js'
+import { createOCR } from '../components/ocr.js'
 
 createHeader(document.querySelector('body'))
+createOCR(document.querySelector('body'))
 createNav(document.querySelector('body'))
