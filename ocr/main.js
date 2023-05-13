7a23b593be4f42e3d564daeb2114b045188d0c26
Christine Brovkina <christine@whateverism.de>
2023-05-13 16:44:57 +0200

    Style OCR and landing pages

--- ocr/main.js
+++ ocr/main.js
@@ -4,5 +4,5 @@ import { createNav } from '../components/navigation.js'
 import { createOCR } from '../components/ocr.js'
 
 createHeader(document.querySelector('body'))
-createOCR(document.querySelector('body'))
+createOCR(document.querySelector('main'))
 createNav(document.querySelector('body'))
