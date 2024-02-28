b05c01cc45ec81986814dd8d6c10aab04824cef7
Christine Brovkina <christine@whateverism.de>
2024-02-28 17:05:19 +0100

    Expand placeholders and pangrams lists

--- resolution/main.js
+++ resolution/main.js
@@ -22,8 +22,8 @@ const fontsizeCustom = document.querySelector("#demo-font-size-custom");
 const resultBox = document.querySelector(".demo-result");
 const axisInput = document.querySelector("#demo-axis");
 
-const placeholders = ["Very easy to work these to death The very fact that youre aware of suffering is enough reason to be overjoyed that you're alive and can experience it Let's put some happy trees and bushes back in here I'm gonna start with a little Alizarin crimson and a touch of Prussian blue I think there's an artist hidden in the bottom of every single one of us God gave you this gift of imagination Use it", "Placeholder 3"];
-const pangrams = ["The quick brown fox jumps over a lazy dog", "Two driven jocks help fax my big quiz", "Sixty zippers were quickly picked from the woven jute bag", "How vexingly quick daft zebras jump", "Pack my box with five dozen liquor jugs"]
+const placeholders = ["Bold Balance","Cursive Creations", "Geometric Grotesque", "Sleek Sans Serif", "Whimsical Script", "Geometric Gems", "Serif Symphony", "Italic Intermezzo","Calligraphic charm", "Symmetrical Serenity", "Modern Monospace", "Dynamic Fontscape", "Vintage Vibes", "Harmonious Hues","Expressive Elements", "Subtle Strokes", "Whitespace Whispers", "Refined Rhythms", "Jovial Juxtapositions", "Noble Notches", "Angular Accents", "Playful Pictograms"];
+const pangrams = ["The quick brown fox jumps over a lazy dog", "Two driven jocks help fax my big quiz", "Sixty zippers were quickly picked from the woven jute bag", "How vexingly quick daft zebras jump", "Pack my box with five dozen liquor jugs","Grumpy wizards make toxic brew for the evil queen and jack", "DJs flock by when MTV ax quiz prog", "Woven silk pyjamas exchanged for blue quartz", "Prating jokers quizzically vexed me with fibs"]
 
 
 textInput.addEventListener("change", (event) => {
