1d3aa565eb2d6dde6f047300e215be612f953c48
Christine Brovkina <christine@whateverism.de>
2024-03-06 17:43:48 +0100

    Add link to homepage to the header

--- components/header.js
+++ components/header.js
@@ -5,6 +5,7 @@ export function createHeader(element) {
     <header>
     <h1>I!&3_OCR</h1>
     <p>typographic experiments on human-only legibility</p>
+    <a class="header-link" href="/index.html">Homepage</a>
     </header>
     `)
 }
