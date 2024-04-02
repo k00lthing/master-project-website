28b0218cb9957762945a18472c7e073e458a8b2a
Christine Brovkina <christine@whateverism.de>
2024-04-02 16:26:17 +0200

    Load pangrams and trsting phrases from json

--- resolution/main.js
+++ resolution/main.js
@@ -3,6 +3,8 @@ import { createHeader } from '../components/header.js'
 import { createNav } from '../components/navigation.js'
 import { createLightbox } from '../components/lightbox.js'
 import Glide from '@glidejs/glide'
+import * as placeholders from '../src/placeholders.json'
+import * as pangrams from '../src/pangrams.json'
 
 createHeader(document.querySelector('body'))
 createNav(document.querySelector('body'))
@@ -22,8 +24,6 @@ const fontsizeCustom = document.querySelector("#demo-font-size-custom");
 const resultBox = document.querySelector(".demo-result");
 const axisInput = document.querySelector("#demo-axis");
 
-const placeholders = ["Bold Balance","Cursive Creations", "Geometric Grotesque", "Sleek Sans Serif", "Whimsical Script", "Geometric Gems", "Serif Symphony", "Italic Intermezzo","Calligraphic charm", "Symmetrical Serenity", "Modern Monospace", "Dynamic Fontscape", "Vintage Vibes", "Harmonious Hues","Expressive Elements", "Subtle Strokes", "Whitespace Whispers", "Refined Rhythms", "Jovial Juxtapositions", "Noble Notches", "Angular Accents", "Playful Pictograms"];
-const pangrams = ["The quick brown fox jumps over a lazy dog", "Two driven jocks help fax my big quiz", "Sixty zippers were quickly picked from the woven jute bag", "How vexingly quick daft zebras jump", "Pack my box with five dozen liquor jugs","Grumpy wizards make toxic brew for the evil queen and jack", "DJs flock by when MTV ax quiz prog", "Woven silk pyjamas exchanged for blue quartz", "Prating jokers quizzically vexed me with fibs"]
 
 
 textInput.addEventListener("change", (event) => {
@@ -31,11 +31,11 @@ textInput.addEventListener("change", (event) => {
 });
 
 placeholderTrigger.addEventListener("click", (event) => {
-    resultBox.textContent = placeholders[Math.floor(Math.random() * placeholders.length)];
+    resultBox.textContent = placeholders.data[Math.floor(Math.random() * placeholders.data.length)];
 });
 
 pangramTrigger.addEventListener("click", (event) => {
-    resultBox.textContent = pangrams[Math.floor(Math.random() * pangrams.length)];
+    resultBox.textContent = pangrams.data[Math.floor(Math.random() * pangrams.data.length)];
 });
 
 fontsizeDropdown.addEventListener("change", (event) => {
