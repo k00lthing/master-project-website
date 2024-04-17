dcbd6ed6a17ae6c487ba9b4b7b14cc3e0a5b3624
Christine Brovkina <christine@whateverism.de>
2024-04-17 14:09:48 +0200

    Setup disorientation

--- disorientation/main.js
+++ disorientation/main.js
@@ -1,6 +1,66 @@
 import '../style.scss'
 import { createHeader } from '../components/header.js'
 import { createNav } from '../components/navigation.js'
+import { createLightbox } from '../components/lightbox.js'
+import Glide from '@glidejs/glide'
+import * as placeholders from '../src/placeholders.json'
+import * as pangrams from '../src/pangrams.json'
 
 createHeader(document.querySelector('body'))
 createNav(document.querySelector('body'))
+
+new Glide('.glide.concept').mount()
+new Glide('.glide.test').mount()
+
+createLightbox(document.querySelector('body'))
+
+const root = document.documentElement;
+const form = document.querySelector("form.font-controls");
+const textInput = document.querySelector("#demo-text-input");
+const placeholderTrigger = document.querySelector("#placeholder");
+const pangramTrigger = document.querySelector("#pangram");
+const fontsizeDropdown = document.querySelector("#demo-font-size-dropdown");
+const fontsizeCustom = document.querySelector("#demo-font-size-custom");
+const resultBox = document.querySelector(".demo-result");
+const axisInputX = document.querySelector("#demo-axis-x");
+const axisInputY = document.querySelector("#demo-axis-y");
+
+
+
+textInput.addEventListener("change", (event) => {
+    resultBox.textContent = `${event.target.value}`;
+});
+
+placeholderTrigger.addEventListener("click", (event) => {
+    resultBox.textContent = placeholders.data[Math.floor(Math.random() * placeholders.data.length)];
+});
+
+pangramTrigger.addEventListener("click", (event) => {
+    resultBox.textContent = pangrams.data[Math.floor(Math.random() * pangrams.data.length)];
+});
+
+fontsizeDropdown.addEventListener("change", (event) => {
+    resultBox.style.fontSize = `${event.target.value}px`;
+});
+
+fontsizeCustom.addEventListener("change", (event) => {
+    resultBox.style.fontSize = `${event.target.value}px`;
+});
+
+axisInputX.addEventListener("change", (event) => {
+    root.style.setProperty('--disorientation-font-axis-x', event.target.value);
+
+});
+
+axisInputY.addEventListener("change", (event) => {
+    root.style.setProperty('--disorientation-font-axis-y', event.target.value);
+
+});
+
+
+form.onreset = () => {
+    resultBox.textContent = pangrams[Math.floor(Math.random() * pangrams.length)];
+    root.style.setProperty('--disorientation-font-axis-x', 0);
+    root.style.setProperty('--disorientation-font-axis-y', 0);
+    resultBox.style.fontSize = '5rem'
+}
